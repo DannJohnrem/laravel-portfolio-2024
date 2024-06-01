f382f3ce6623143de36133dcc4c4492550851ab3
DannJohnrem <jonrhem10@gmail.com>
2024-06-02 00:51:52 +0800

    fix navigation UI and add hero section

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -25,102 +25,59 @@ const isAuthenticated = page.props.auth.user !== null;
 
 <template>
     <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
-        <div class="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="relative flex items-center justify-between h-16">
-                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                    <!-- Mobile menu button-->
-                    <DisclosureButton
-                        class="relative inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                        <span class="absolute -inset-0.5" />
-                        <span class="sr-only">Open main menu</span>
-                        <Bars3Icon v-if="!open" class="block w-6 h-6" aria-hidden="true" />
-                        <XMarkIcon v-else class="block w-6 h-6" aria-hidden="true" />
-                    </DisclosureButton>
-                </div>
-                <div class="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
-                    <div class="flex items-center flex-shrink-0">
-                        <img class="w-auto h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                            alt="Your Company" />
-                        <span class="text-white">
-                            Dann Johnrem
-                        </span>
+        <header>
+            <div class="max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
+                <div class="flex items-center justify-between h-16">
+                    <div class="md:flex md:items-center md:gap-12">
+                        <a class="block text-teal-600" href="#">
+                            <span class="sr-only">Home</span>
+                            <img class="w-auto h-8"
+                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                                alt="Your Company" />
+                        </a>
                     </div>
-                </div>
-                <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <div class="hidden sm:ml-6 sm:block">
-                        <div class="flex space-x-4">
-                            <a v-for="item in navigation" :key="item.name" :href="item.href"
-                                :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']"
-                                :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
-                        </div>
+                    <div class="hidden md:block">
+                        <nav aria-label="Global">
+                            <ul class="flex items-center gap-6 text-sm">
+                                <li>
+                                    <a v-for="item in navigation" :key="item.name" :href="item.href"
+                                        :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white transition', 'rounded-md px-3 py-2 text-sm font-medium']"
+                                        :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
-                </div>
-                <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <template v-if="isAuthenticated">
-                        <!-- Notifications button -->
-                        <button type="button"
-                            class="relative p-1 text-gray-400 bg-gray-800 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                            <span class="absolute -inset-1.5" />
-                            <span class="sr-only">View notifications</span>
-                            <BellIcon class="w-6 h-6" aria-hidden="true" />
-                        </button>
 
-                        <!-- Profile dropdown -->
-                        <Menu as="div" class="relative ml-3">
-                            <div>
-                                <MenuButton
-                                    class="relative flex text-sm bg-gray-800 rounded-full focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                                    <span class="absolute -inset-1.5" />
-                                    <span class="sr-only">Open user menu</span>
-                                    <img class="w-8 h-8 rounded-full"
-                                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                        alt="User Profile Image" />
-                                </MenuButton>
-                            </div>
-                            <transition enter-active-class="transition duration-100 ease-out"
-                                enter-from-class="transform scale-95 opacity-0"
-                                enter-to-class="transform scale-100 opacity-100"
-                                leave-active-class="transition duration-75 ease-in"
-                                leave-from-class="transform scale-100 opacity-100"
-                                leave-to-class="transform scale-95 opacity-0">
-                                <MenuItems
-                                    class="absolute right-0 z-10 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                    <MenuItem v-slot="{ active }">
-                                    <a href="#"
-                                        :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Your
-                                        Profile</a>
-                                    </MenuItem>
-                                    <MenuItem v-slot="{ active }">
-                                    <a href="#"
-                                        :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Settings</a>
-                                    </MenuItem>
-                                    <MenuItem v-slot="{ active }">
-                                    <a href="#"
-                                        :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign
-                                        out</a>
-                                    </MenuItem>
-                                </MenuItems>
-                            </transition>
-                        </Menu>
-                    </template>
-
-                    <template v-else>
-                        <div class="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
+                    <div class="flex items-center gap-4">
+                        <div class="sm:flex sm:gap-4">
                             <Link :href="route('login')"
-                                class="px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-md">
-                            Log in</Link>
+                                class="rounded-md bg-gray-900 px-5 py-2.5 text-sm font-medium text-white shadow">
+                            Login
+                            </Link>
 
-                            <Link v-if="canRegister" :href="route('register')"
-                                class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">
-                            Register</Link>
+                            <div class="hidden sm:flex">
+                                <Link v-if="canRegister" :href="route('register')"
+                                    class="rounded-md hover:bg-gray-700 hover:text-white px-5 py-2.5 text-sm font-medium text-gray-300">
+                                Register
+                                </Link>
+                            </div>
                         </div>
-                    </template>
+
+                        <div class="block md:hidden">
+                            <!-- Mobile menu button-->
+                            <DisclosureButton
+                                class="relative inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                <span class="absolute -inset-0.5" />
+                                <span class="sr-only">Open main menu</span>
+                                <Bars3Icon v-if="!open" class="block w-6 h-6" aria-hidden="true" />
+                                <XMarkIcon v-else class="block w-6 h-6" aria-hidden="true" />
+                            </DisclosureButton>
+                        </div>
+                    </div>
                 </div>
-
             </div>
-        </div>
-
-        <DisclosurePanel class="sm:hidden">
+        </header>
+        <DisclosurePanel class="md:hidden">
             <div class="px-2 pt-2 pb-3 space-y-1">
                 <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href"
                     :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,21 +1,107 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
-import Footer from '@/Components/Footer.vue';
-import Navbar from '@/Components/Navbar.vue';
+import { Head, Link } from "@inertiajs/vue3";
+import Footer from "@/Components/Footer.vue";
+import Navbar from "@/Components/Navbar.vue";
 
 defineProps({
     canLogin: Boolean,
     canRegister: Boolean,
-    auth: Object
+    auth: Object,
 });
-
 </script>
 
 <template>
+
     <Head title="DannPortfolio" />
     <Navbar :canLogin="canLogin" :canRegister="canRegister"></Navbar>
-    <div class="relative min-h-screen bg-gray-100 bg-center sm:flex sm:justify-center sm:items-center bg-dots-darker dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
 
+    <section class="bg-dots-darker dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+        <div class="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+            <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+                <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+                    <img alt=""
+                        src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                        class="absolute inset-0 object-cover w-full h-full" />
+                </div>
+
+                <div class="lg:py-24">
+                    <h2 class="text-3xl font-bold sm:text-4xl dark:text-white">Grow your audience</h2>
+
+                    <p class="mt-4 text-gray-600 dark:text-gray-300">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
+                        eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae eius
+                        quidem quam repellat.
+                    </p>
+
+                    <a href="#"
+                        class="inline-block px-12 py-3 mt-8 text-sm font-medium text-white transition bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400">
+                        Get Started Today
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- <section class="overflow-hidden bg-dots-darker dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white sm:grid sm:grid-cols-2">
+        <div class="p-8 md:p-12 lg:px-16 lg:py-24">
+            <div class="max-w-xl mx-auto text-center ltr:sm:text-left rtl:sm:text-right">
+                <h2 class="text-2xl font-bold text-gray-900 dark:text-white md:text-3xl">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit
+                </h2>
+
+                <p class="hidden text-gray-500 md:mt-4 md:block">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam
+                    sed. Quam a scelerisque amet ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat
+                    quisque ut interdum tincidunt duis.
+                </p>
+
+                <div class="mt-4 md:mt-8">
+                    <a href="#"
+                        class="inline-block px-12 py-3 text-sm font-medium text-white transition bg-gray-800 rounded hover:bg-gray-700 focus:outline-none focus:ring focus:ring-yellow-400">
+                        Get Started Today
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <img alt=""
+            src="https://images.unsplash.com/photo-1464582883107-8adf2dca8a9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            class="object-cover w-full h-56 sm:h-full" />
+    </section> -->
+    <!-- <section
+        class="relative bg-dots-darker dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+        <div class="relative max-w-screen-xl px-4 py-32 mx-auto lg:flex lg:h-screen lg:items-start">
+            <div class="max-w-xl mx-auto text-center dark:text-white lg:z-10">
+                <h1 class="text-3xl font-extrabold sm:text-5xl">
+                    Understand User Flow.
+                    <strong class="font-extrabold text-red-700 sm:block">
+                        Increase Conversion.
+                    </strong>
+                </h1>
+
+                <p class="mt-4 sm:text-xl/relaxed">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Nesciunt illo tenetur fuga ducimus numquam ea!
+                </p>
+
+                <div class="flex flex-wrap justify-center gap-4 mt-8">
+                    <a class="block w-full px-12 py-3 text-sm font-medium text-white bg-red-600 rounded shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+                        href="#">
+                        Get Started
+                    </a>
+
+                    <a class="block w-full px-12 py-3 text-sm font-medium text-red-600 rounded shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+                        href="#">
+                        Learn More
+                    </a>
+                </div>
+            </div>
+            <img alt=""
+                src="https://images.unsplash.com/photo-1464582883107-8adf2dca8a9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                class="absolute top-0 right-0 object-cover w-full h-full lg:w-1/2 lg:h-full" />
+        </div>
+    </section> -->
+    <div
+        class="relative min-h-screen bg-gray-100 bg-center sm:flex sm:justify-center sm:items-center bg-dots-darker dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
     </div>
     <Footer></Footer>
 </template>
@@ -24,6 +110,7 @@ defineProps({
 .bg-dots-darker {
     background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
 }
+
 @media (prefers-color-scheme: dark) {
     .dark\:bg-dots-lighter {
         background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
